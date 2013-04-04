9fa20216def20b7ac54f76f1ebb627373e727c4b
aluno <aluno@192.168.10.27>
2013-04-04 16:37:39 -0300

    Primeira classe - Musica - adicionada.

--- proj4tons/src/br/unesp/coral/Musica.java
+++ proj4tons/src/br/unesp/coral/Musica.java
@@ -9,6 +9,13 @@ package br.unesp.coral;
  * @author aluno
  */
 public class Musica {
+	
+	private String nome;
+	private String cantada;
+	private String playback;
+	private String partitura;
+	private String letra;
+	private String kit;
 
 	public Musica() {
 	}
